8a34b7a0ec501d75b888c43b22d996473715cf0c
aaya622-spec <aaya622@gmail.com>
2026-09-08 23:20:21 +0900

    Create manifest.ts

---
+++ catchcatchbo/app/manifest.ts
@@ -0,0 +1,34 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "캐치캐치보",
+    short_name: "캐치캐치보",
+
+    description:
+      "친구와 편하게 약속을 잡는 예약 서비스",
+
+    start_url: "/book",
+
+    display: "standalone",
+
+    background_color: "#FFF9F3",
+
+    theme_color: "#FFF9F3",
+
+    orientation: "portrait",
+
+    icons: [
+      {
+        src: "/icon-192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icon-512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
